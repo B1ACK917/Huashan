53ef1dd40dc6343f46a4e7d3ee818352575dc313
B1ACK917 <397615103@qq.com>
2023-12-15 23:45:42 +0800

    add: --file option

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from utils.uploader import Akira
+import argparse
+
+parser = argparse.ArgumentParser(description="Akira parser")
+parser.add_argument("-f", "--file", type=str, help='Path to file')
 
 if __name__ == '__main__':
     token = "<token>"
     cookie = "<cookie>"
     akira = Akira(token, cookie)
+
+    args = parser.parse_args()
     akira.upload("sample.txt")
+    akira.upload(args.file)
